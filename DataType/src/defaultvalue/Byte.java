73e91044ab193630aa936e48b648cfe9b85d7ca5
Kshitijagadekar <kshitijatajane.11@gmail.com>
2025-05-04 19:48:55 +0530

    new commit

---
+++ DataType/src/defaultvalue/Byte.java
@@ -0,0 +1,36 @@
+package defaultvalue;
+
+public class Byte {
+
+	static boolean booleandefaultvalue;
+	static	byte bytedefaultvalue;
+	static short shortdefaultvalue;
+	static int intdefaultvalue;
+	static long longdefaultvalue;
+	static float floatdefaultvalue;
+	static double doubledefaultvalue;
+	static char chardefaultvalue;
+		
+	public static void main (String[] args) {
+                  
+	System.out.println("boolean defalut = " + booleandefaultvalue );
+	
+	System.out.println("byte default = " + bytedefaultvalue);
+	
+	System.out.println("short defalut = " + shortdefaultvalue );
+
+	System.out.println("int defalut = " + intdefaultvalue );
+
+	System.out.println("long defalut = " + longdefaultvalue );
+
+	System.out.println("float defalut = " + floatdefaultvalue );
+	
+	System.out.println("double defalut = " + doubledefaultvalue );
+	
+	System.out.println("char defalut = " + chardefaultvalue );
+
+}
+	
+	
+	
+}
---
+++ DataType/src/defaultvalue/Task.java
@@ -0,0 +1,36 @@
+package defaultvalue;
+
+public class Task {
+	
+ 
+	  public static void main(String[] args) {
+		  
+		 // boolean B = true; cannot convert boolean to byte
+		  
+		 // byte b = B;
+		   byte b1= 100;
+		  		
+		   short s = b1;
+		
+		   int i = s;
+		   
+		   long l = i;
+		   
+		   float f = l;
+		   
+		   double d = f;
+		   
+		  //System.out.println("boolean value = " + B);
+		  System.out.println("byte value = " + b1);
+		  System.out.println("short value = " + s);
+		  System.out.println("int value = " + i);
+		  System.out.println("long value = " + l);
+		  System.out.println("float value = " + f);
+		  System.out.println("double value = " + d);
+
+
+
+
+	  }
+	  
+}
